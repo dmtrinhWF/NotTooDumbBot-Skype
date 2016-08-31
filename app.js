c98efc210043c7dd8749444160aca7596e145c55
Duc M. Trinh <duc.m.trinh@gmail.com>
2016-08-30 22:40:08 -0700

    Added LUIS integration .  Minor cleanup for readability.

--- app.js
+++ app.js
@@ -41,40 +41,100 @@ bot.beginDialogAction('help', '/help', { matches: /^help/i });
 // Bots Dialogs
 //=========================================================
 
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', dialog);
-
-// Add intent handlers
-dialog.onDefault([
-    function (session, args, next) {
-        session.send("You said: %s", session.message.text);
-        session.send("I'm sorry, I don't know how to handle this yet. My Master only taught me a couple conversation skills so far. LOL");
+bot.dialog('/', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("NotTooDumbBot")
+            .text("beep beep beeeeeeeeeeep!")
+            .images([
+                 builder.CardImage.create(session, 
+                 "https://idigitalcitizen.files.wordpress.com/2009/07/autobots-red1.jpg?w=256")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("This Not-too-dumb-Bot's main purpose is simply to " +
+            "testdrive Microsoft Bot Framework so I can better understand how " +
+            "it works.  The bot currently implements the following: \n\n" +
+            "* A simple help menu\n" +
+            "* A set of global action handlers\n" +
+            "* Integration with Microsoft LUIS\n so it can have basic language understanding\n\n" +
+            "The bot makes use of the ChatConnector which allows it to interface with a" +
+            "variety of channels including Skype."
+        );
+        session.beginDialog('/help');
+    },
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // When does this actually get called?!
+        session.send("Hasta la vista!");
     }
 ]);
 
-dialog.matches('builtin.intent.places.show_map',
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("== HELP BOX ==\n\n" +
+            "Global commands that can be invoked anytime:\n" +
+            "* menu - Display main menu\n" +
+            "* quit - Quit the current conversation\n" +
+            "* help - Bring up this help box"
+        );
+    }
+]);
+
+bot.dialog('/menu', [
+    function (session) {
+        builder.Prompts.choice(session, "Choose an option:", "LUIS|(quit)");
+    },
+    function (session, results) {
+        if (results.response && results.response.entity != '(quit)') {
+            if (results.response.entity == 'LUIS');
+                session.send("LUIS testdrive... start chatting with the bot!");
+            session.beginDialog('/' + results.response.entity);
+        } else {
+            // Exit the menu
+            session.endDialog();
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/menu');
+    }
+]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+
+var intent_dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/LUIS', intent_dialog);
+
+// Add intent handlers
+intent_dialog.onDefault([
+    function (session, args, next) {
+        session.send("You said: %s", session.message.text);
+        session.send("I'm sorry, I don't know how to handle this yet. Ducmeister only taught me a couple conversation skills so far. LOL");
+    }
+]);
+
+intent_dialog.matches('builtin.intent.places.show_map',
     function (session, args, next) {
         // Resolve entities passed from LUIS.
         session.send("I think you are trying to find where you are...");
-        session.send("Unfortunately, my Master has not taught me how to call Google Maps yet to bring back some results for you.");
+        session.send("Unfortunately, Ducmeister has not taught me how to call Google Maps yet to bring back some results for you.");
     }
 );
 
-dialog.matches('builtin.intent.places.find_place', [
+intent_dialog.matches('builtin.intent.places.find_place', [
     function (session, args, next) {
         session.send("I think you are searching for one or more places...");
         // Resolve and store any entities passed from LUIS.
         var place_type = builder.EntityRecognizer.findEntity(args.entities, 'builtin.places.place_type');
         session.send("\ttype of place: %s", place_type != null ? place_type.entity : "Unknown");
-        session.send("Unfortunately, my Master has not taught me how to call Google yet to bring back some results for you.");
-        next();
-    },
-    function (session, results) {
-        session.send("=== end processing.");
+        session.send("Unfortunately, Ducmeister has not taught me how to call Google yet to bring back some results for you.");
     }
 ]);
 
-dialog.matches('builtin.intent.weather.check_weather', [
+intent_dialog.matches('builtin.intent.weather.check_weather', [
     function (session, args, next) {
         session.send("I think you are trying to check the weather...");
         // Resolve and store any entities passed from LUIS.
@@ -82,10 +142,17 @@ dialog.matches('builtin.intent.weather.check_weather', [
         session.send("\ttype of place: %s", place_type != null ? place_type.entity : "None provided...  Assuming your current location.");
         var date_range = builder.EntityRecognizer.findEntity(args.entities, 'builtin.weather.date_range');
         session.send("\tdate(s): %s", date_range != null ? date_range.entity : "None provided.");
-        session.send("Unfortunately, my Master has not taught me how to call Weather Underground yet to bring back some results for you.");
+        session.send("Unfortunately, Ducmeister has not taught me how to call Weather Underground yet to bring back some results for you.");
         next();
     },
     function (session, results) {
-        session.send("=== end processing.");
+        session.send("=== end waterfall.");
     }
 ]);
+
+intent_dialog.matches(/\b(quit|end|exit)\b/,
+    function (session, args, next) {
+        // Resolve entities passed from LUIS.
+        session.endDialog("OK.  Bye!");
+    }
+);
