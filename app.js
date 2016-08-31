7a87eb3f66f4726d32aaf98689d6034afe927249
Duc M. Trinh <duc.m.trinh@gmail.com>
2016-08-30 23:47:12 -0700

    Added index.html

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
